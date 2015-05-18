c7e3f2e154d790ac1307647d49541d41fd9260be
Gustavo Tavares Dias <gustavotavaresdias@gmail.com>
2015-05-18 13:15:15 +0800

    Update questionnaire, question, unit controllers
    Linked questions to questionnaires and questionnaire to units

--- QuizApp/QuizApp/Controllers/LecturersController.cs
+++ QuizApp/QuizApp/Controllers/LecturersController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using QuizApp.DAL;
 using QuizApp.Models;
+using QuizApp.ViewModels;
+using System.Data.Entity.Infrastructure;
 
 namespace QuizApp.Controllers
 {
@@ -39,23 +41,34 @@ namespace QuizApp.Controllers
         // GET: Lecturers/Create
         public ActionResult Create()
         {
+            var lecturer = new Lecturer();
+            lecturer.Units = new List<Unit>();
+            PopulateAssignedUnitData(lecturer);
             return View();
         }
 
         // POST: Lecturers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,LecturerNumber,FirstName,LastName,Email,Password,IsAdm")] Lecturer lecturer)
+        public ActionResult Create([Bind(Include = "LecturerNumber,FirstName,LastName,Email,Password,IsAdm")] Lecturer lecturer, string[] selectedUnits)
         {
+            if (selectedUnits != null)
+            {
+                lecturer.Units = new List<Unit>();
+                foreach (var unit in selectedUnits)
+                {
+                    var unitToAdd = db.Units.Find(int.Parse(unit));
+                    lecturer.Units.Add(unitToAdd);
+                }
+            }
+            
             if (ModelState.IsValid)
             {
                 db.Lecturers.Add(lecturer);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
+            PopulateAssignedUnitData(lecturer);
             return View(lecturer);
         }
 
@@ -66,7 +79,14 @@ namespace QuizApp.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Lecturer lecturer = db.Lecturers.Find(id);
+            Lecturer lecturer = db.Lecturers
+                .Include(i => i.Units)
+                .Where(i => i.ID == id)
+                .Single();
+            
+            PopulateAssignedUnitData(lecturer);
+            
+            //Lecturer lecturer = db.Lecturers.Find(id);
             if (lecturer == null)
             {
                 return HttpNotFound();
@@ -79,16 +99,77 @@ namespace QuizApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LecturerNumber,FirstName,LastName,Email,Password,IsAdm")] Lecturer lecturer)
+        public ActionResult Edit(int? id, string[] selectedUnits)
         {
-            if (ModelState.IsValid)
+            if (id == null)
             {
-                db.Entry(lecturer).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            return View(lecturer);
+            var lecturerToUpdate = db.Lecturers
+               .Include(i => i.Units)
+               .Where(i => i.ID == id)
+               .Single();
+
+            if (TryUpdateModel(lecturerToUpdate, "",
+               new string[] { "LecturerNumber" , "FirstName" , "LastName" , "Email" , "Password" , "IsAdm" }))
+            {
+                try
+                {
+                    UpdateLecturerUnits(selectedUnits, lecturerToUpdate);
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            PopulateAssignedUnitData(lecturerToUpdate);
+            return View(lecturerToUpdate);
         }
+
+        private void UpdateLecturerUnits(string[] selectedUnits, Lecturer lecturerToUpdate)
+        {
+            if (selectedUnits == null)
+            {
+                lecturerToUpdate.Units = new List<Unit>();
+                return;
+            }
+
+            var selectedUnitsHS = new HashSet<string>(selectedUnits);
+            var instructorCourses = new HashSet<int>
+                (lecturerToUpdate.Units.Select(c => c.ID));
+            foreach (var unit in db.Units)
+            {
+                if (selectedUnitsHS.Contains(unit.ID.ToString()))
+                {
+                    if (!instructorCourses.Contains(unit.ID))
+                    {
+                        lecturerToUpdate.Units.Add(unit);
+                    }
+                }
+                else
+                {
+                    if (instructorCourses.Contains(unit.ID))
+                    {
+                        lecturerToUpdate.Units.Remove(unit);
+                    }
+                }
+            }
+        }
+        //public ActionResult Edit([Bind(Include = "ID,LecturerNumber,FirstName,LastName,Email,Password,IsAdm")] Lecturer lecturer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(lecturer).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(lecturer);
+        //}
 
         // GET: Lecturers/Delete/5
         public ActionResult Delete(int? id)
@@ -124,5 +205,23 @@ namespace QuizApp.Controllers
             }
             base.Dispose(disposing);
         }
+
+        private void PopulateAssignedUnitData(Lecturer lecturer)
+        {
+            var allUnits = db.Units;
+            var lecturerUnits = new HashSet<int>(lecturer.Units.Select(c => c.ID));
+            var viewModel = new List<AssignedUnitData>();
+            foreach (var unit in allUnits)
+            {
+                viewModel.Add(new AssignedUnitData
+                {
+                    UnitID = unit.ID,
+                    Title = unit.Name,
+                    Assigned = lecturerUnits.Contains(unit.ID)
+                });
+            }
+            ViewBag.Units = viewModel;
+        }
+
     }
 }
